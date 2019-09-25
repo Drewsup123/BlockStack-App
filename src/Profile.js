9e8e9ff40ca1b2450b989e38fe2e88ae1230387c
Drew <drewsup123c@gmail.com>
2019-09-24 23:35:05 -0600

    restyled profile page

--- src/Profile.js
+++ src/Profile.js
@@ -2,10 +2,34 @@ import React, { Component } from 'react';
 import {
   Person,
 } from 'blockstack';
+import { withStyles } from '@material-ui/styles';
+import PropTypes from 'prop-types';
 
 const avatarFallbackImage = 'https://s3.amazonaws.com/onename/avatar-placeholder.png';
 
-export default class Profile extends Component {
+const styles = {
+  container : {
+    display : "flex",
+    flexDirection : "column",
+    justifyContent : "center",
+    alignItems : "center"
+  },
+  logoutBtn : {
+    height : "100px",
+    width : "200px",
+    cursor : "pointer",
+    borderRadius : "10px",
+    background : "blue",
+    color : "white",
+    "&:hover" : {
+      background : "white",
+      color : "blue",
+      transition : "0.5s"
+    }
+  }
+}
+
+class Profile extends Component {
   constructor(props) {
   	super(props);
 
@@ -29,25 +53,32 @@ export default class Profile extends Component {
   }
 
   render() {
-    const { handleSignOut, userSession } = this.props;
+    const { handleSignOut, userSession, classes } = this.props;
     const { person } = this.state;
     return (
       !userSession.isSignInPending() ?
-      <div className="panel-welcome" id="section-2">
+      <div className={classes.container}>
         <div className="avatar-section">
           <img src={ person.avatarUrl() ? person.avatarUrl() : avatarFallbackImage } className="img-rounded avatar" id="avatar-image" alt=""/>
         </div>
         <h1>Hello, <span id="heading-name">{ person.name() ? person.name() : 'Nameless Person' }</span>!</h1>
         <p className="lead">
           <button
-            className="btn btn-primary btn-lg"
+            className={classes.logoutBtn}
             id="signout-button"
             onClick={ handleSignOut.bind(this) }
           >
             Logout
           </button>
         </p>
-      </div> : null
+      </div> 
+      : null
     );
   }
 }
+
+Profile.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Profile)
